2a400f28e50d6cd2ac71f724dee54b4a04005c0b
VeraLizunova <vera.lizunova.98@mail.ru>
2022-07-19 16:30:57 +0300

    added PostRepository

---
+++ src/main/java/repository/PostRepository.java
@@ -0,0 +1,54 @@
+package repository;
+
+import model.Post;
+import validator.PostValidator;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class PostRepository {
+    private final static int NEW_POST_INDEX = 0;
+    private final static int START_POST_INDEX = 1;
+    private final List<Post> posts = new CopyOnWriteArrayList<>();
+    private long counter = START_POST_INDEX;
+
+    public List<Post> all() {
+        PostValidator.validate(posts);
+        return posts;
+    }
+
+    public Optional<Post> getById(long id) {
+        for (var post : posts) {
+            if (post.getId() == id) {
+                return Optional.of(post);
+            }
+            break;
+        }
+        return Optional.empty();
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == NEW_POST_INDEX) {
+            addNewPost(post);
+        } else {
+            updatePost(post);
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        var existingPost = PostValidator.validate(posts, id);
+        posts.remove(existingPost);
+    }
+
+    private void addNewPost(Post post) {
+        post.setId(counter++);
+        posts.add(post);
+    }
+
+    private void updatePost(Post post) {
+        var existingPost = PostValidator.validate(posts, post);
+        existingPost.setContent(post.getContent());
+    }
+}
