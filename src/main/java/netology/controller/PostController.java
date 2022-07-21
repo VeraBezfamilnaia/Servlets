6d2b3299053830c184c7c675a7892e1eac301fc3
VeraLizunova <vera.lizunova.98@mail.ru>
2022-07-21 19:59:27 +0300

    rewritten using Spring config

--- src/main/java/netology/controller/PostController.java
+++ src/main/java/netology/controller/PostController.java
@@ -1,13 +1,15 @@
-package controller;
+package netology.controller;
 
 import com.google.gson.Gson;
-import model.Post;
-import service.PostService;
+import netology.model.Post;
+import org.springframework.stereotype.Controller;
+import netology.service.PostService;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Reader;
 
+@Controller
 public class PostController {
     private static final String APPLICATION_JSON = "application/json";
     private final PostService service;
